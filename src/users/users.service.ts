ce5e187833911d972be4ecd73412cff7555158f0
losefor <moh999157@gmail.com>
2021-11-18 16:21:28 +0300

    add: users services

---
+++ src/users/users.service.ts
@@ -0,0 +1,26 @@
+
+import { Prisma, User } from '.prisma/client'
+import {prisma} from '../prisma/prisma.service'
+
+export const reshape = (users : User[] )=>{
+    return users.map(({password , ...safe})=> safe)
+}
+
+
+export const findMany = async (args : Prisma.UserFindManyArgs)=>{
+    const users = await prisma.user.findMany(args)
+    return reshape(users)
+}
+
+// findUniq 
+export const findUnique = async (args : Prisma.UserFindUniqueArgs)=>{
+    const user = await prisma.user.findUnique(args)
+    const reshapedUser =  reshape([user as User])
+    return reshapedUser
+}
+
+export const findUniqueUnsafe = async (args : Prisma.UserFindUniqueArgs)=>{
+    const users = await prisma.user.findUnique(args)
+    return users
+}
+
