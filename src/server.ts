a74c8aa7e3a0ad474ad13f6cf48bd08c328db5ca
losefor <moh999157@gmail.com>
2021-11-19 20:13:47 +0300

    fix: log

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,6 @@ import 'express-async-errors';
 import { secrets } from './utils/config';
 import { app } from './app';
 
-console.log(secrets);
-
 // Start listening
 app.listen(secrets.PORT, () => {
   console.log(`💻 App listening on port ${secrets.PORT}`);
