2fd6428c5a19020c4e30c8375db1bd0d28a31ae4
losefor <moh999157@gmail.com>
2021-11-20 09:28:50 +0300

    fix: bug with naming

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import 'express-async-errors';
-import { secrets } from './utils/config';
+import { config } from './utils/config';
 import { app } from './app';
 
 // Start listening
-const expressServer = app.listen(secrets.PORT, () => {
-  console.log(`💻 App listening on port ${secrets.PORT}`);
+const expressServer = app.listen(config.PORT, () => {
+  console.log(`💻 App listening on port ${config.PORT}`);
 });
 
 export { express };
