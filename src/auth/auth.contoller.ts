6969855b5a22a6016892d3bcee0ee6e405bdcf25
losefor <moh999157@gmail.com>
2021-11-21 18:59:16 +0300

    add: auth controller

--- src/auth/auth.contoller.ts
+++ src/auth/auth.contoller.ts
@@ -1,1 +1,17 @@
-import jwt from 'jsonwebtoken';
+import { User } from '.prisma/client';
+import { Request, Response } from 'express';
+import * as authService from './auth.service';
+
+export const login = async (
+  req: Request,
+  res: Response
+): Promise<Response<any>> => {
+  return res.json(authService.reshape(req.user as User));
+};
+
+export const signup = async (
+  req: Request,
+  res: Response
+): Promise<Response<any>> => {
+  return res.json(authService.reshape(req.user as User));
+};
